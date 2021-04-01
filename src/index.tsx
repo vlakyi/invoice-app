54673910b9b14b08de199d464db80e2266f1014f
Vlad <kiyashko.vlad1@gmail.com>
2021-04-01 20:19:34 +0200

    Add basic styling configuration. _reset.scss with css reset, _root.scss with custom css variables and main.scss for global styling

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './components/App/App';
-import './styles/style.scss';
+import './styles/main.scss';
 
 ReactDOM.render(
   <React.StrictMode>
